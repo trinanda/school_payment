bed589ecd68f95e83a0716498b17171f5bb8888e
Tri Nanda <zidanecr7kaka@gmail.com>
2019-01-25 23:09:19 +0700

    excluded columns student_registration form in student edit/add flask admin

--- app/modelview.py
+++ app/modelview.py
@@ -1,9 +1,15 @@
+import random
+import string
+
 from flask import abort, redirect, url_for, request
 from flask_admin.contrib import sqla
 from sqlalchemy import func
 from flask_security import current_user
 from app.models import Student
 
+
+def generator_random(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
 
 class SuperUserModelView(sqla.ModelView):
 
@@ -55,12 +61,12 @@ class ParentModelView(SchoolAdminModelView):
     pass
 
 
-class StudentModelView(sqla.ModelView):
+class StudentModelView(SchoolAdminModelView):
     def get_query(self):
-        return Student.query.filter_by(school_id=current_user.id)
-        # return self.session.query(self.model).filter(
-        #     Student.school_id == current_user.id
-        # )
+        # return Student.query.filter_by(school_id=current_user.id)
+        return self.session.query(self.model).filter(
+            Student.school_id == current_user.id
+        )
 
     def get_count_query(self):
         return self.session.query(func.count('*')).select_from(self.model).filter(
@@ -70,7 +76,9 @@ class StudentModelView(sqla.ModelView):
     def on_model_change(self, form, model, is_created):
         if is_created:
             model.school_id = current_user.id
+            model.student_registration_number = generator_random()
 
+    form_excluded_columns = ('student_registration_number')
 
 class BillModelView(SchoolAdminModelView):
     column_list = ('student_id', 'total_bill', 'bill_status')
